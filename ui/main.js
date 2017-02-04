fc0af04aefacfd99ec2742662bc36a3fe0f339f5
Karthik Suresh <karthiks2601@gmail.com>
2017-02-04 15:58:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,11 @@ element.innerHTML='New Value';
 //Move the image
 var img=document.getElementById('madi');
 var marginLeft=0;
-img.onclick = function(){
-    var interval=setinterval(moveRight,100);
-  
-};
 function moveRight(){
     marginLeft+=10;
     img.style.marginLeft=marginLeft+ "px";
 }
+img.onclick = function(){
+    var interval=setInterval(moveRight,100);
+  
+};
